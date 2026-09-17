fe0a7d629d78f3d417763099ab6508f2cef74199
Vido-Faresky <vidogamingmax@gmail.com>
2026-09-17 09:12:42 +0700

    feat: implement class-based status-badge and back routes

--- app/View/Components/StatusBadge.php
+++ app/View/Components/StatusBadge.php
@@ -8,12 +8,10 @@ use Illuminate\View\Component;
 
 class StatusBadge extends Component
 {
-    /**
-     * Create a new component instance.
-     */
-    public function __construct()
+    public string $status;
+    public function __construct(string $status)
     {
-        //
+        $this->status = $status;
     }
 
     /**
